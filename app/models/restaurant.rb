4e22273686ae88b2238c6ce0d436191e03d8d7c9
Valentin Cadel <104843541+valencadel@users.noreply.github.com>
2024-10-15 21:32:55 -0300

    yelp-mvp a medias

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: {in: CATEGORIES}
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Limpiando base de datos...'
+
+Restaurant.destroy_all
+
+puts 'Creando restaurantes....'
+Restaurant.create(name: "Epicure", address: "75008 Paris", phone_number: "0123456789", category: "french")
+Restaurant.create(name: "Café Tortoni", address: "Av. de Mayo 825, Buenos Aires", phone_number: "011-4342-4328", category: "chinese")
+Restaurant.create(name: "El Cuartito", address: "Talcahuano 937, Buenos Aires", phone_number: "011-4816-1758", category: "italian")
+Restaurant.create(name: "Bar El Federal", address: "Carlos Calvo 599, Buenos Aires", phone_number: "011-4300-4313", category: "japanese")
+Restaurant.create(name: "El Gaucho Parrilla", address: "Av. Corrientes 1234, Buenos Aires", phone_number: "011-4567-8901", category: "belgian")
+
+puts 'Terminamos de cargar los restaurantes'
